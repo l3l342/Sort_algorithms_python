25a6c2aa8f0685d2c3b322ea65e3572d4216a907
Ben <75950537+l3l342@users.noreply.github.com>
2022-04-20 22:37:34 +0200

    Add files via upload

---
+++ Quicksort.py
@@ -0,0 +1,26 @@
+#quicksort algorithmus ~Sebastion -> der pisser, Beni, Paulus
+
+unsorted_list = [4,6,8,15,4,3, -6, 84,5]
+
+def quicksort(List): #Die List
+    if len(List) <= 1:
+        return List   
+
+    pivot = List.pop()
+    
+    shorter_List = []
+    greater_List = []
+
+    for elem in List:
+        if elem < pivot:
+            shorter_List.append(elem)
+        else:
+            greater_List.append(elem)
+    return quicksort(shorter_List) + [pivot] + quicksort(greater_List)
+
+
+a = quicksort(unsorted_list)
+print(a)
+
+
+#nimm zahl in lite
